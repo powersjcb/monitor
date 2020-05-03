21af82c1a0030da980d21611a234d6167460394a
Jacob Powers <jpowers@23andme.com>
2020-05-03 09:18:57 -0700

    remove gcp instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,33 +8,6 @@ resource "random_id" "instance_id" {
   byte_length = 8
 }
 
-resource "google_compute_instance" "default" {
-  name = "monitor-vm-${random_id.instance_id.hex}"
-  machine_type = "f1-micro"
-  zone = var.zone
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
-    }
-  }
-  metadata_startup_script = "echo 'hello world'"
-  metadata = {
-    ssh-keys = "powersjcb:${file("~/.ssh/jacobpowers_personal_macbook.pub")}"
-  }
-
-  network_interface {
-    network = "default"
-    // default network config to avoid setting up Cloud NAT
-    access_config {
-    }
-  }
-}
-
-output "ip" {
-  value = google_compute_instance.default.network_interface.0.access_config.0.nat_ip
-}
-
 // allow load balancers access to instance
 resource "google_compute_firewall" "firewall" {
   name = "monitor-firewall"
