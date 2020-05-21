91e97ec31f1c9bb0ef0638044506fcbfd14ed1d7
Jacob Powers <jpowers@23andme.com>
2020-05-20 17:49:50 -0700

    update sqlInterceptor

--- go/src/lib/tracer/db.go
+++ go/src/lib/tracer/db.go
@@ -18,20 +18,81 @@ func OpenDB(dsn string) (*sql.DB, error) {
 	return db, err
 }
 
-type sqlInterceptor struct {
-	sqlmw.NullInterceptor
-}
+type sqlInterceptor struct {}
 
 // ref: https://github.com/open-telemetry/opentelemetry-specification/blob/master/specification/trace/semantic_conventions/database.md
+const dbKind = "db.kind"
+const dbInstance = "db.instance"
+const dbStatement = "db.statement"
+const dbUser = "db.user"
+
+const defaultQueryLabel = "postgres"
+const defaultDBUser = "app"
+const dbKindValue = "sql"
+
+func (sqlInterceptor) ConnBeginTx(ctx context.Context, conn driver.ConnBeginTx, txOpts driver.TxOptions) (driver.Tx, error) {
+	span := trace.SpanFromContext(ctx)
+	queryLabel := getCallerName()
+	if queryLabel == "" {
+		queryLabel = defaultQueryLabel
+	}
+	ctx, dbSpan := span.Tracer().Start(ctx, queryLabel, trace.WithSpanKind(trace.SpanKindClient))
+	dbSpan.SetAttributes(
+		kv.String(dbKind, dbKindValue),
+		kv.String(dbInstance, ""),
+		kv.String(dbStatement, "BEGIN"),
+		kv.String(dbUser, defaultDBUser),
+	)
+	defer dbSpan.End()
+	return conn.BeginTx(ctx, txOpts)
+}
+
+func (sqlInterceptor) ConnPrepareContext(ctx context.Context, conn driver.ConnPrepareContext, query string) (driver.Stmt, error) {
+	span := trace.SpanFromContext(ctx)
+	queryLabel := getCallerName()
+	if queryLabel == "" {
+		queryLabel = defaultQueryLabel
+	}
+	ctx, dbSpan := span.Tracer().Start(ctx, queryLabel, trace.WithSpanKind(trace.SpanKindClient))
+	dbSpan.SetAttributes(
+		kv.String(dbKind, dbKindValue),
+		kv.String(dbInstance, ""),
+		kv.String(dbStatement, query), // todo: determine what data is provided in query for preparing a statement
+		kv.String(dbUser, defaultDBUser),
+	)
+	defer dbSpan.End()
+	return conn.PrepareContext(ctx, query)
+}
+
+func (sqlInterceptor) ConnPing(ctx context.Context, conn driver.Pinger) error {
+	span := trace.SpanFromContext(ctx)
+	queryLabel := getCallerName()
+	if queryLabel == "" {
+		queryLabel = defaultQueryLabel
+	}
+	ctx, dbSpan := span.Tracer().Start(ctx, queryLabel, trace.WithSpanKind(trace.SpanKindClient))
+	dbSpan.SetAttributes(
+		kv.String(dbKind, dbKindValue),
+		kv.String(dbInstance, ""),
+		kv.String(dbStatement, "PING"),
+		kv.String(dbUser, defaultDBUser),
+	)
+	defer dbSpan.End()
+	return conn.Ping(ctx)
+}
+
 func (sqlInterceptor) ConnExecContext(ctx context.Context, conn driver.ExecerContext, query string, args []driver.NamedValue) (driver.Result, error) {
 	span := trace.SpanFromContext(ctx)
-	// assumes query does not contain any values
-	ctx, dbSpan := span.Tracer().Start(ctx, "postgres-helpful-query-name", trace.WithSpanKind(trace.SpanKindClient))
+	queryLabel := getCallerName()
+	if queryLabel == "" {
+		queryLabel = defaultQueryLabel
+	}
+	ctx, dbSpan := span.Tracer().Start(ctx, queryLabel, trace.WithSpanKind(trace.SpanKindClient))
 	dbSpan.SetAttributes(
-		kv.String("db.kind", "sql"),
-		kv.String("db.instance", ""),
-		kv.String("db.statement", query),
-		kv.String("db.user", ""),
+		kv.String(dbKind, dbKindValue),
+		kv.String(dbInstance, ""),
+		kv.String(dbStatement, query),
+		kv.String(dbUser, defaultDBUser),
 	)
 	defer dbSpan.End()
 	return conn.ExecContext(ctx, query, args)
@@ -41,17 +102,124 @@ func (sqlInterceptor) ConnQueryContext(ctx context.Context, conn driver.QueryerC
 	span := trace.SpanFromContext(ctx)
 	queryLabel := getCallerName()
 	if queryLabel == "" {
-		queryLabel = "postgres-query"
+		queryLabel = defaultQueryLabel
 	}
 	ctx, dbSpan := span.Tracer().Start(ctx, queryLabel, trace.WithSpanKind(trace.SpanKindClient))
 	dbSpan.SetAttributes(
-		kv.String("db.kind", "sql"),
-		kv.String("db.instance", ""),
-		kv.String("db.statement", query),
-		kv.String("db.user", "app"),
+		kv.String(dbKind, dbKindValue),
+		kv.String(dbInstance, ""),
+		kv.String(dbStatement, query),
+		kv.String(dbUser, defaultDBUser),
 	)
 	defer dbSpan.End()
 	return conn.QueryContext(ctx, query, args)
+}
+
+// gets a datatbase conn for a single goroutine
+func (sqlInterceptor) ConnectorConnect(ctx context.Context, connect driver.Connector) (driver.Conn, error) {
+	span := trace.SpanFromContext(ctx)
+	queryLabel := getCallerName()
+	if queryLabel == "" {
+		queryLabel = defaultQueryLabel
+	}
+	ctx, dbSpan := span.Tracer().Start(ctx, queryLabel, trace.WithSpanKind(trace.SpanKindClient))
+	dbSpan.SetAttributes(
+		kv.String(dbKind, dbKindValue),
+		kv.String(dbInstance, ""),
+		kv.String(dbStatement, "CONNECT"),
+		kv.String(dbUser, defaultDBUser),
+	)
+	defer dbSpan.End()
+	return connect.Connect(ctx)
+}
+
+func (sqlInterceptor) ResultLastInsertId(res driver.Result) (int64, error) {
+	return res.LastInsertId()
+}
+
+func (sqlInterceptor) ResultRowsAffected(res driver.Result) (int64, error) {
+	return res.RowsAffected()
+}
+
+func (sqlInterceptor) RowsNext(_ context.Context, rows driver.Rows, dest []driver.Value) error {
+	return rows.Next(dest)
+}
+
+func (sqlInterceptor) StmtExecContext(ctx context.Context, stmt driver.StmtExecContext, query string, args []driver.NamedValue) (driver.Result, error) {
+	span := trace.SpanFromContext(ctx)
+	queryLabel := getCallerName()
+	if queryLabel == "" {
+		queryLabel = defaultQueryLabel
+	}
+	ctx, dbSpan := span.Tracer().Start(ctx, queryLabel, trace.WithSpanKind(trace.SpanKindClient))
+	dbSpan.SetAttributes(
+		kv.String(dbKind, dbKindValue),
+		kv.String(dbInstance, ""),
+		kv.String(dbStatement, query),
+		kv.String(dbUser, defaultDBUser),
+	)
+	defer dbSpan.End()
+	return stmt.ExecContext(ctx, args)
+}
+
+func (sqlInterceptor) StmtQueryContext(ctx context.Context, stmt driver.StmtQueryContext, query string, args []driver.NamedValue) (driver.Rows, error) {
+	span := trace.SpanFromContext(ctx)
+	queryLabel := getCallerName()
+	if queryLabel == "" {
+		queryLabel = defaultQueryLabel
+	}
+	ctx, dbSpan := span.Tracer().Start(ctx, queryLabel, trace.WithSpanKind(trace.SpanKindClient))
+	dbSpan.SetAttributes(
+		kv.String(dbKind, dbKindValue),
+		kv.String(dbInstance, ""),
+		kv.String(dbStatement, query),
+		kv.String(dbUser, defaultDBUser),
+	)
+	defer dbSpan.End()
+	return stmt.QueryContext(ctx, args)
+}
+
+func (sqlInterceptor) StmtClose(ctx context.Context, stmt driver.Stmt) error {
+	span := trace.SpanFromContext(ctx)
+	queryLabel := getCallerName()
+	if queryLabel == "" {
+		queryLabel = defaultQueryLabel
+	}
+	_, dbSpan := span.Tracer().Start(ctx, queryLabel, trace.WithSpanKind(trace.SpanKindClient))
+	dbSpan.SetAttributes(
+		kv.String(dbKind, dbKindValue),
+		kv.String(dbInstance, ""),
+		kv.String(dbStatement, "CLOSE PREPARED STATEMENT"),
+		kv.String(dbUser, defaultDBUser),
+	)
+	defer dbSpan.End()
+	return stmt.Close()
+}
+
+func (sqlInterceptor) TxCommit(ctx context.Context, tx driver.Tx) error {
+	span := trace.SpanFromContext(ctx)
+	_, dbSpan := span.Tracer().Start(ctx, defaultQueryLabel, trace.WithSpanKind(trace.SpanKindClient))
+	dbSpan.SetAttributes(
+		kv.String(dbKind, dbKindValue),
+		kv.String(dbInstance, ""),
+		kv.String(dbStatement, "COMMIT"),
+		kv.String(dbUser, defaultDBUser),
+	)
+	defer dbSpan.End()
+	return tx.Commit()
+}
+
+func (sqlInterceptor) TxRollback(ctx context.Context, tx driver.Tx) error {
+	span := trace.SpanFromContext(ctx)
+	_, dbSpan := span.Tracer().Start(ctx, defaultQueryLabel, trace.WithSpanKind(trace.SpanKindClient))
+	dbSpan.SetAttributes(
+		kv.String(dbKind, dbKindValue),
+		kv.String(dbInstance, ""),
+		kv.String(dbStatement, "ROLLBACK"),
+		kv.String(dbUser, defaultDBUser),
+	)
+	defer dbSpan.End()
+	return tx.Rollback()
 }
 
 var dbLibs = []string{
