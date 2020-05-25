8b1b367dfd4d5ca53dabcb563140fda424432454
Jacob Powers <jpowers@23andme.com>
2020-05-24 22:52:43 -0700

    add span to track client uptime

--- go/cmd/client/main.go
+++ go/cmd/client/main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/jackpal/gateway"
 	"github.com/powersjcb/monitor/go/src/client"
 	"github.com/powersjcb/monitor/go/src/lib/tracer"
+	"go.opentelemetry.io/otel/api/global"
 	"log"
 	"os"
 	"strings"
@@ -16,9 +17,15 @@ import (
 const publicTracingKey = "a0f88ec0416dae30766466ab00f0492c"
 
 func main() {
+	ctx := context.Background()
 	runOnce := flag.Bool("once", false, "Run pings only one time.")
 	flag.Parse()
+
 	tracer.InitTracer(publicTracingKey)
+	t := global.Tracer("monitor.client")
+	_, span := t.Start(ctx, "client.run")
+	defer span.End()
+
 	pingConfigs := client.DefaultPingConfigs
 	gw, err := gateway.DiscoverGateway()
 	if err == nil && gw != nil && gw.String() != "" {
@@ -30,7 +37,6 @@ func main() {
 		fmt.Printf("unable to discover default gateway: %s", err.Error())
 	}
 	h, _ := os.Hostname()
-	ctx := context.Background()
 	apiKey := os.Getenv("MONITOR_API_KEY")
 	if apiKey == "" {
 		fmt.Println("warning invalid api key")
